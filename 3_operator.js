0bea1271c4dc89da027c518e3f1deccb77f3ddce
Lileshmohane1 <98868493+Lileshmohane1@users.noreply.github.com>
2022-08-08 12:11:07 +0530

    Add files via upload

---
+++ 3_operator.js
@@ -0,0 +1,36 @@
+// operaters in js 
+//  arathmetic operater  
+console.log("operater in js ")
+let a= 5;
+let b= 10;
+console.log("a+b =",a+b)
+console.log("a-b =",a-b)
+console.log("a*b =",a*b)
+console.log("a/b =",a/b)
+console.log("a**b =",a**b)
+console.log("a%b =",a%b)
+
+console.log("++a=",++a)
+console.log("a++=",a++)
+console.log("--a =",--a)
+console.log("a--=",a--)
+console.log("a=",a)
+console.log("a--=",a--)
+//o/p = 5,6,6,6,6,5,5
+//  assignment operater 
+a+=10
+a-=10
+a==10
+console.log(a)
+//  comparison operator 
+let c= 2;
+let d= "2";
+console.log("compare",c==d)
+console.log("compare",c!=d)
+console.log("compare",c===d)
+//  logical operator 
+let x= 5;
+let y=10;
+console.log(x<y &&x==y)
+console.log(x<y ||x==y)
+console.log(!false)
