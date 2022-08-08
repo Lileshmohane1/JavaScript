bee16d69422b7a194b85f9205dbdb9dafc3294fc
Lileshmohane1 <98868493+Lileshmohane1@users.noreply.github.com>
2022-08-08 11:05:09 +0530

    Add files via upload

---
+++ 1_variables.js
@@ -0,0 +1,12 @@
+//  variable in js ,
+//  we use var  variable it have some error some issu  so we use a let  variable 
+//  7 type of var 
+let a= 5;  //int 
+let b= null ; // nulla cha
+let c=" lilesh" //  string 
+let d= true;  //  bool 
+let f // undefine 
+let  g= BigInt("23")+BigInt("25")
+// now print a value 
+document.write(a,b,c,d,e,f,g)  
+//console.log(a,b,c,d,e,f,g)s
